37da913f5e80130b1d77dc4612aa2c704cb80bdc
Luciano Dichiara <luciano@studiohakuna.com>
2020-07-16 02:07:18 -0300

    Alert 'Game Over' + CSS adjustments

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ const grid = document.querySelector('.grid');
 const btnPlay = document.querySelector('.btn-play');
 const btnReset = document.querySelector('.btn-reset');
 const btnLamp = document.querySelector('.btn-lamp');
+const scoreCounter = document.querySelector('.scoreCounter');
 
 const frames = ['Angular', 'React', 'Vue', 'Svelte', 'Ember', 'Backbone'];
 let cardsSelected = [];
 let pairsFound = 0;
+let score = 12;
 
 
 // EVENT LISTENERS
@@ -23,49 +25,61 @@ btnLamp.addEventListener('click', turnOnTheLight);
 function tapCard(e) {
     e.preventDefault();
     //console.log(e.target.classList);
+    console.log(cardsSelected);
 
     if ((e.target.classList.contains('front')) || (e.target.classList.contains('back'))) {
 
+        // Delete index in cardSelected
         if (e.target.parentElement.classList.contains('active')) {
 
             let index = cardsSelected.indexOf(e.target.parentElement.id);
-
             e.target.parentElement.classList.remove('active');
-
             cardsSelected.splice(index, 1);
 
         } else {
 
             if (cardsSelected.length != 2) {
-
                 e.target.parentElement.classList.add('active');
-
                 cardsSelected.push(e.target.parentElement.id);
-
             }
+
         }
 
     }
 
-    if (checkPairs(cardsSelected)) {
 
+    if (checkPairs(cardsSelected)) {
         const activeCards = document.querySelectorAll('.box.active');
 
         activeCards.forEach(function (e) {
-
             e.classList.add('disable');
-
         });
 
         pairsFound++;
-        console.log(pairsFound);
+        console.log('Pares encontrados: ' + pairsFound);
 
         cardsSelected = [];
-
     }
 
-    showAlertWin(pairsFound);
+    calculateScore(e);
+    printScore();
 
+    /*
+    if ((!checkPairs(cardsSelected)) && (cardsSelected.length === 2)) {
+        const active = document.querySelectorAll('.box.active:not(.disable)');
+
+        setTimeout(function () {
+            active.forEach(function (e) {
+                e.classList.add('active');
+            });
+        }, 1000)
+
+        cardsSelected = [];
+    }
+    */
+
+    showAlertWin(pairsFound);
+    showAlertGameOver(score)
 };
 
 function createCards(array) {
@@ -82,6 +96,26 @@ function createCards(array) {
             </div>
         `;
         }, i * 80)
+    }
+}
+
+function showAlertGameOver(score) {
+    if (score === 0) {
+        setTimeout(function () {
+
+            Swal.fire({
+                title: 'Game Over!',
+                text: 'You lost the game.',
+                icon: 'error',
+                confirmButtonText: 'Play again!'
+            }).then((result) => {
+                if (result.value) {
+                    resetGame();
+                    play();
+                }
+            })
+
+        }, 500)
     }
 }
 
@@ -102,7 +136,6 @@ function showAlertWin(count) {
 
 function turnOnTheLight() {
     const body = document.querySelector('body');
-
     body.classList.toggle("on");
 }
 
@@ -124,9 +157,26 @@ function checkPairs(array) {
     return new Set(array).size !== array.length
 }
 
+function calculateScore(e) {
+    e.preventDefault();
+    //console.log(e.target.classList);
+    if ((e.target.classList.contains('back'))) {
+        if (!e.target.parentElement.classList.contains('active')) {
+            score = score - 1;
+        }
+    }
+}
+
+function printScore() {
+    scoreCounter.innerHTML = `You have <strong>${score} chances</strong>`;
+}
+
 function resetGame() {
     grid.innerHTML = "";
+    cardsSelected = [];
     pairsFound = 0;
+    score = 18;
+    scoreCounter.innerHTML = "";
     btnReset.disabled = true;
     btnPlay.disabled = false;
 }
@@ -136,6 +186,7 @@ function play() {
     cards = duplicatePairs(frames);
     cards = sortPairs(cards);
     createCards(cards);
+    printScore();
     cards = [];
 
     btnPlay.disabled = true;
